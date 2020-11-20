aabe14b294cb7acbc0e18bf8165e22c790d03df9
Maxx Kozec <kozecm@oregonstate.edu>
2020-11-19 18:15:18 -0800

    contact page working

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -6,16 +6,31 @@
           Contact
         </label>
         <br />
+        <br />
         <div class="columns">
-          <div class="column">
-            Email: <a href="mailto:maxxkozec@hotmail.com"> maxxkozec@hotmail.com</a>
-          </div>
-          <div class="column">
-            <figure class="image is-3by1">
+          <div class="column"></div>
+          <div class="column is-centered">
+            <a class="button is-large is-primary" v-bind:href="'mailto:maxxkozec@hotmail.com'">
+              <span class="icon is-medium">
+                <font-awesome-icon :icon="mailIcon" size='1x'/>
+              </span>
+              <span> Email</span> 
+            </a>
+            <br />
+            <br />
+            <figure class="image">
               <a href="https://www.linkedin.com/in/maxx-kozec-185b9482/" target="_blank">
                 <img id="image" src="../../public/LinkedIn.png" >
               </a>
             </figure>
+            <br />
+            <br />
+            <a class="button is-large is-success" v-bind:href="'https://github.com/KozecM'" v-bind:target="'_blank'">
+              <span class="icon is-medium">
+                <font-awesome-icon :icon="gitIcon" />
+              </span>
+              <span> Github</span> 
+            </a>
           </div>
           <div class="column"></div>
         </div>
@@ -27,6 +42,8 @@
 
 <script>
 /*eslint-disable*/
+  import {faEnvelope} from '@fortawesome/free-regular-svg-icons'
+  import {faGithub} from '@fortawesome/free-brands-svg-icons'
 
   export default {
     data() {
@@ -40,6 +57,14 @@
         
       },
     },
+    computed: {
+      mailIcon () {
+        return faEnvelope
+      },
+      gitIcon () {
+        return faGithub
+      }
+    }
   };
 /*eslint -enable*/
 </script>
@@ -55,5 +80,7 @@ h3 {
 #image {
   max-width: 70%;
   max-height: 40%;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import bulma from './../node_modules/bulma/css/bulma.css';
 import router from "./routes";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
