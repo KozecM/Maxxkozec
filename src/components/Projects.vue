e8a338167fc3de851a9723ee391c6734f7d88e92
Maxx Kozec <kozecm@oregonstate.edu>
2021-01-09 18:45:50 -0800

    updated dist

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -17,45 +17,46 @@
         <div class="columns">
           <div class="column"></div>
           <div class="column">
-            <p class="title is-family-sans-serif is-centered">Who am I? </p>
-            <p class="is-family-sans-serif">Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-              sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-          <div class="column"></div>
-        </div>
-      </div>
-    </div>
-    <br />
-    <div class="card has-background-grey-lighter">
-      <div class="card-content">
-        <div class="columns">
-          <div class="column"></div>
-          <div class="column">
-            <p class="title is-family-sans-serif is-centered">What I Can Do.</p>
-            <p class="is-family-sans-serif">Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-              sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-          <div class="column"></div>
-        </div>
-      </div>
-    </div>
-    <br />
-    <div class="card has-background-grey-lighter">
-      <div class="card-content">
-        <div class="columns">
-          <div class="column"></div>
-          <div class="column">
-            <p class="title is-family-sans-serif is-centered">I'm Here to Help.</p>
-            <p class="is-family-sans-serif">Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-              sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p class="title is-family-sans-serif is-centered">Scott Bot</p>
+            <p class="is-family-sans-serif">This is a very simple Bot that is desinged to allow one person (Scott) to give people stars on my discord server. It also does a few other things 
+              like repeating what was said and outputting random 99 quotes. I plan on desining a website that displays the current star rankings for each discord server that is hosting the bot.
+            </p>
             <br />
-            <a class="button" id="email"  v-bind:href="'mailto:' + email">Message Me</a>
+            <a class="button" id="email"  v-bind:href="'https://github.com/KozecM/ScottBot/tree/develop'">View Code</a>
+          </div>
+          <div class="column"></div>
+        </div>
+      </div>
+    </div>
+    <br />
+    <div class="card has-background-grey-lighter">
+      <div class="card-content">
+        <div class="columns">
+          <div class="column"></div>
+          <div class="column">
+            <p class="title is-family-sans-serif is-centered">Energy Assistance Application</p>
+            <p class="is-family-sans-serif">This is a simple application for a non profit that is designed to get information to decided if a person is eligible for assistance with their energy bills.
+              It uses firebase as a backend and VueJS on the frontend. It is very flexible and works on most devices (hasn't been tested on fridge screens)
+            </p>
+            <br />
+            <a class="button" id="email"  v-bind:href="'https://github.com/KozecM/Energy_Form'">View Code</a>
+            <a class="button" id="email"  v-bind:href="'https://energy-assistance-application.web.app/'">View Site</a>
+          </div>
+          <div class="column"></div>
+        </div>
+      </div>
+    </div>
+    <br />
+    <div class="card has-background-grey-lighter">
+      <div class="card-content">
+        <div class="columns">
+          <div class="column"></div>
+          <div class="column">
+            <p class="title is-family-sans-serif is-centered">Webmail and pdf creation for Sgioba</p>
+            <p class="is-family-sans-serif">This project was a part of some freelance work I did for a company based in france making an app for councellors in universities. I worked in PHP and JavaScript in order to work with legacy code and 
+              add new, required features. Unfortunatly I am not able to show the code I work on because of the private and sensitive data that I was working with. 
+            </p>
+            <br />
           </div>
           <div class="column"></div>
         </div>
